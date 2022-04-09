31a7d3ce78d45bdb3b316648d4c9a20f326f18f5
bkprog <73074013+bkprog@users.noreply.github.com>
2022-04-09 16:23:20 +0200

    initial commit

---
+++ src/main/java/dietplaner/example/dietplaner/DietPlanerApplication.java
@@ -0,0 +1,13 @@
+package dietplaner.example.dietplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietPlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietPlanerApplication.class, args);
+	}
+
+}
